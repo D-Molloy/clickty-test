93e4296b7f3a1057d3e6c4bd17fdf8d7463be9b6
Denis Molloy <denis.molloy@gmail.com>
2019-11-01 10:19:33 -0700

    Updates

---
+++ precache-manifest.dd038dc8873176fefcbae18bb4aaabe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6bdc506cd5c21f9b35079ab887031d3",
+    "url": "/clickty-test/index.html"
+  },
+  {
+    "revision": "f91c26e3cc54cb2f9fff",
+    "url": "/clickty-test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ffeac9b860feeaada9b4",
+    "url": "/clickty-test/static/js/2.013541e5.chunk.js"
+  },
+  {
+    "revision": "f91c26e3cc54cb2f9fff",
+    "url": "/clickty-test/static/js/main.4f049247.chunk.js"
+  },
+  {
+    "revision": "de15bebe968101cced92",
+    "url": "/clickty-test/static/js/runtime-main.0f235119.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/clickty-test/static/media/logo.25bf045c.svg"
+  }
+]);
